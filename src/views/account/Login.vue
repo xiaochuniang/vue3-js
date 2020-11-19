0751cd00d6b72da4af6557e32f9fd0baf103e8e2
qx <qx@metgs.com>
2020-11-19 18:20:44 +0800

    初始化

---
+++ src/views/account/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login-main">
+    <a-form
+      name="custom-validation"
+      ref="ruleForm"
+    >
+      <a-form-item>
+        <label>用户名</label>
+        <a-input type="text" autocomplete="off" />
+      </a-form-item>
+      <a-form-item>
+        <label>密码</label>
+        <a-input type="password" autocomplete="off" />
+      </a-form-item>
+
+      <a-form-item>
+        <a-button type="primary" block>
+          Primary
+        </a-button>
+      </a-form-item>
+      
+    </a-form>
+  </div>
+</template>
+
+<script>
+import {} from 'vue'
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  .login-main{
+    width: 100vw;
+    height: 100vh;
+    background-color: cornflowerblue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    label{
+      color: #FFF;
+    }
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devDist',
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "./src/styles/main.scss";`
+      },
+    }
+  }
+}
